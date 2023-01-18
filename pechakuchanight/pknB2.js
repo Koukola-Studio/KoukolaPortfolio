3a1f55d281e45d3aed13dd756ec31f6afe7c33b6
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-01-18 15:33:42 +0100

    Add files via upload

---
+++ pechakuchanight/pknB2.js
@@ -0,0 +1,7 @@
+let pknB2 = bodymovin.loadAnimation({
+    container: document.getElementById('pknB2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'pechaKuchaB2.json'
+})
