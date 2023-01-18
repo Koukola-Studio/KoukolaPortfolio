837d50fc522ca372840d4c49a4b498dd01342b09
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-01-18 15:34:39 +0100

    Add files via upload

---
+++ cz/cestovatelskyfestival/tFestival.js
@@ -0,0 +1,7 @@
+let animationFestivalBrandys = bodymovin.loadAnimation({
+    container: document.getElementById('tFestival'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'travellingFestival.json'
+})
---
+++ cz/cestovatelskyfestival/tFestival2.js
@@ -0,0 +1,7 @@
+let animationFestivalBrandys2 = bodymovin.loadAnimation({
+    container: document.getElementById('tFestival2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'travellingFestival2.json'
+})
