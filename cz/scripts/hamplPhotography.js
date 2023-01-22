0f73160631050117672c1238d436e098ee9aeb00
Adam Koukola <66322408+koukolaadam@users.noreply.github.com>
2023-01-22 19:23:34 +0100

    Add files via upload

---
+++ cz/scripts/hamplPhotography.js
@@ -0,0 +1,7 @@
+let hamplPhotography = bodymovin.loadAnimation({
+    container: document.getElementById('hamplPhotography'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'hamplPhotography.json'
+})
